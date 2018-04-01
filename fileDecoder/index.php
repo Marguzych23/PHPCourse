4d04cdaf83c692df06b87b8faf417fcb8cfe0a79
Marguzych <zaynullin.emil@gmail.com>
2018-04-02 00:52:36 +0300

    fix 6th home task

--- fileDecoder/index.php
+++ fileDecoder/index.php
@@ -14,7 +14,7 @@ function decode_by_first_rules(string $upper, string &$string)
     if ($upper == "true") {
         return strtoupper($string);
 //        return mb_strtoupper($string);
-    } else if ($upper == "false") {
+    } elseif ($upper == "false") {
         return strtolower($string);
 //        return mb_strtolower($string);
     }
@@ -33,7 +33,7 @@ function decode_by_second_rules(string $direction, string &$string)
                 } else {
                     $char = 0;
                 }
-            } else if ($direction == "-") {
+            } elseif ($direction == "-") {
                 if ($char != 0) {
                     $char--;
                 } else {
@@ -59,41 +59,49 @@ function get_rules(array $config)
     global $file;
 
     $errors = [];
-
     $symbols_and_rules = Array();
+    $rule_number = 0;
 
     $file = $config["main"]["filename"];
 
-    $first_rule = $config["first_rule"];
-    $second_rule = $config["second_rule"];
-    $third_rule = $config["third_rule"];
-
-    if ($first_rule["upper"] != "true" and $first_rule["upper"] != "false") {
-        array_push($errors, "Invalid character for the first rule - " . $first_rule["upper"]);
-    } else {
-        $symbols_and_rules[$first_rule["symbol"]] = array(
-            "rule" => 1,
-            "char" => $first_rule["upper"],
-        );
-    }
-
-    if ($second_rule["direction"] != "+" and $second_rule["direction"] != "-") {
-        array_push($errors, "Invalid character for the second rule - " . $second_rule["direction"]);
-    } else {
-
-        $symbols_and_rules[$second_rule["symbol"]] = array(
-            "rule" => 2,
-            "char" => $second_rule["direction"],
-        );
-    }
-
-    if (strlen($third_rule["delete"]) != 1) {
-        array_push($errors, "Invalid character for the third rule - " . $third_rule["delete"] . " because length" . strlen($third_rule["delete"]) . " != 1");
-    } else {
-        $symbols_and_rules[$third_rule["symbol"]] = array(
-            "rule" => 3,
-            "char" => $third_rule["delete"],
-        );
+    foreach ($config as $rule) {
+        if (isset($rule["filename"])) {
+            $rule_number++;
+            continue;
+        } elseif (isset($rule["upper"])) {
+            if ($rule["upper"] != "true" and $rule["upper"] != "false") {
+                array_push($errors, $rule_number . ". Invalid character for the first rule - " . $rule["upper"]);
+            } else {
+                $symbols_and_rules[] = array(
+                    "symbol" => $rule["symbol"],
+                    "rule" => 1,
+                    "char" => $rule["upper"],
+                );
+            }
+        } elseif (isset($rule["direction"])) {
+            if ($rule["direction"] != "+" and $rule["direction"] != "-") {
+                array_push($errors, $rule_number . ". Invalid character for the second rule - " . $rule["direction"]);
+            } else {
+                $symbols_and_rules[] = array(
+                    "symbol" => $rule["symbol"],
+                    "rule" => 2,
+                    "char" => $rule["direction"],
+                );
+            }
+        } elseif (isset($rule["delete"])) {
+            if (strlen($rule["delete"]) != 1) {
+                array_push($errors, $rule_number . ". Invalid character for the third rule - " . $rule["delete"] . " because length" . strlen($rule["delete"]) . " != 1");
+            } else {
+                $symbols_and_rules[] = array(
+                    "symbol" => $rule["symbol"],
+                    "rule" => 3,
+                    "char" => $rule["delete"],
+                );
+            }
+        } else {
+            array_push($errors, $rule_number . ". Invalid rule");
+        }
+        $rule_number++;
     }
 
     if (!empty($errors)) {
@@ -103,36 +111,45 @@ function get_rules(array $config)
     return $symbols_and_rules;
 }
 
-function echo_decode_data_from_file($file, array $rules)
+function decode_data_from_file($file, array $rules)
 {
     $data = file($file, FILE_IGNORE_NEW_LINES);
 
-    foreach ($data as $line) {
-        $char = $line[0];
-        if (isset($rules[$char])) {
-            switch ($rules[$char]["rule"]) {
-                case 1:
-                    echo decode_by_first_rules($rules[$char]["char"], $line);
-                    break;
-                case 2:
-                    echo decode_by_second_rules($rules[$char]["char"], $line);
-                    break;
-                case 3:
-                    echo decode_by_third_rules($rules[$char]["char"], $line);
-                    break;
-                default:
-                    echo $line;
-                    break;
+    foreach ($rules as $rule) {
+        $first_symbols_length = strlen($rule["symbol"]);
+        for ($i = 0; $i < count($data); $i++) {
+            $line = $data[$i];
+            if (substr($line, 0, $first_symbols_length) == $rule["symbol"]) {
+                switch ($rule["rule"]) {
+                    case 1:
+                        $data[$i] = decode_by_first_rules($rule["char"], $line);
+                        break;
+                    case 2:
+                        $data[$i] = decode_by_second_rules($rule["char"], $line);
+                        break;
+                    case 3:
+                        $data[$i] = decode_by_third_rules($rule["char"], $line);
+                        break;
+                    default:
+//                        $data[$i] = $line;
+                        break;
+                }
             }
-        } else {
-            echo $line;
         }
-        echo "<br>";
+    }
+
+    return $data;
+}
+
+function echo_array_data(array $data)
+{
+    foreach ($data as $line) {
+        echo $line . "<br>";
     }
 }
 
 $rules = get_rules($config);
 
-echo_decode_data_from_file($file, $rules);
+echo_array_data(decode_data_from_file($file, $rules));
 
 ?>
